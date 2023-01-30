4f0a8748d2075c936f5659707dac2d6375260e21
Gucci Fan <gucc@umich.edu>
2023-01-29 21:33:19 -0500

    first working fetch from backend

--- src/Shops.js
+++ src/Shops.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ShopCard from './ShopCard'
 
 // map through shops from backend
@@ -6,9 +6,22 @@ import ShopCard from './ShopCard'
 
 function Shops(){
 
-    //fetch shops
+    const [shops, setShops] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:9292/shops")
+        .then(response=>response.json())
+        .then(response=>{
+          setShops(response)
+      })
+    })
+
+    //fetch shops from localhost:9292
     return(
         <div>map through shops
+            {shops.map(shop =>(
+                shop.name
+            ))}
             <ShopCard />
         </div>
     )
